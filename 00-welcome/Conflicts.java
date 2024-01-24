1b613c1fafd524bf332ed6c1e519000a9d8a2902
Anderson Bittencourt <anderson_btt@hotmail.com>
2024-01-24 15:11:41 -0300

    Naming Conflicts

---
+++ 00-welcome/Conflicts.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+//import java.util.*;
+//import java.sql.Date;
+//import java.sql.*;
+
+public class Conflicts {
+	Date date;
+	//java.util.Date date;
+	java.sql.Date sqlDate;
+}
