a160c48c8c9d872cfb9bbfa3849b5920b4366db7
Anderson Bittencourt <anderson_btt@hotmail.com>
2024-01-24 14:48:20 -0300

    Redundant Imports

--- 00-welcome/ImportExample.java
+++ 00-welcome/ImportExample.java
@@ -1,3 +1,6 @@
+import java.lang.System; // redundant since java.lang package is imported automatically
+import java.lang.*; //redundant since java.lang package is imported automatically
+import java.util.Random; // redundant since java.util.* is already imported below
 import java.util.*; // imports java.util.Random among other things
 
 public class ImportExample{
