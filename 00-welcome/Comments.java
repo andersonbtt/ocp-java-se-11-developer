56d92484953071499244658f7359903313dd723d
Anderson Bittencourt <anderson_btt@hotmail.com>
2024-01-24 14:33:38 -0300

    Comments

--- 00-welcome/Comments.java
+++ 00-welcome/Comments.java
@@ -1,22 +1,30 @@
 public class Comments {
 	// comment until end of line
+	
 	/* Multiple
 	 * line
 	 * comment
 	 */
+	
 	/** 
 	 * Javadoc
 	 * multiple-line
 	 * comment
 	 * @author Anderson
 	 */
+	
 	/*
 	 * // Multiple line
 	 */
-	// single line
+	
+	// single line	
+	
 	// // still single line
+
 	// /* still single line */
+
 	/* multiple lines */
+
 	/*
 	 * /* doesn't compile */
 	// */
