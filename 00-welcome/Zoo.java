534e1b8de1811cd77a309eced01ce71be9c44a8f
Anderson Bittencourt <anderson_btt@hotmail.com>
2024-01-24 14:36:05 -0300

    Creating main() method

--- 00-welcome/Zoo.java
+++ 00-welcome/Zoo.java
@@ -1,5 +1,7 @@
 public class Zoo{
-	public static void main(String[] args){
+		public static void main(String args []){
+	//	public static void main(String... args){
+	//	public static void main(String[] args){
 		
 	}
 }
