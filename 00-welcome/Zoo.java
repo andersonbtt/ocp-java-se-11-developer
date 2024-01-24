2c03bdb3ca297db79fc0c52ecea60f29cbaf5823
Anderson Bittencourt <anderson_btt@hotmail.com>
2024-01-24 14:38:04 -0300

    Passing parameters to a Java program

--- 00-welcome/Zoo.java
+++ 00-welcome/Zoo.java
@@ -2,6 +2,7 @@ public class Zoo{
 		public static void main(String args []){
 	//	public static void main(String... args){
 	//	public static void main(String[] args){
-		
+		System.out.println(args[0]);
+		System.out.println(args[1]);
 	}
 }
